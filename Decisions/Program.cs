21ef472606b3a73dc5a0aaa81b9f380feee5dfef
Jayden Bishop <jbpi8607@gmail.com>
2021-10-21 11:27:08 -0400

    hi

--- Decisions/Program.cs
+++ Decisions/Program.cs
@@ -38,9 +38,12 @@ namespace Decisions
             string userValue = Console.ReadLine();
 
             string message = (userValue == "1") ? "boat" : "strand of lint";
-            Console.Write("You won a ");
-            Console.Write(message);
-            Console.Write(".");
+            // Console.Write("You won a ");
+            // Console.Write(message);
+            // Console.Write(".");
+            
+            Console.WriteLine("You won a {0}.", message);
+            
             Console.ReadLine();
 
 
