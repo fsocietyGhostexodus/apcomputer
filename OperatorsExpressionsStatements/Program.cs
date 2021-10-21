ce89963b3d364b074f4dd9f481128061e181e415
Jayden Bishop <jbpi8607@gmail.com>
2021-10-21 11:44:20 -0400

    hi

---
+++ OperatorsExpressionsStatements/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OperatorsExpressionsStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable decleration
+            int x, y, a, b;
+
+            // Assignment Operator
+            x = 3;
+            y = 2;
+            a = 1;
+            b = 0;
+
+            // There are many mathematical operators ..
+
+            // Addition operator
+            x = 3 + 4;
+
+            // Subtraction operator
+            x = 4 - 3;
+
+            // Mutliplication operator 
+            x = 10 * 5;
+
+            // Division operater
+            x = 10/5;
+
+            // Order of operations using parthesis
+            x =  (x + y) * (a - b);
+
+               
+        }
+    }
+}
