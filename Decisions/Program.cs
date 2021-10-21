92e52cd17f60bb74aae78b7ebd5608e0df9cf419
Jayden Bishop <jbpi8607@gmail.com>
2021-10-21 11:18:30 -0400

    hi

--- Decisions/Program.cs
+++ Decisions/Program.cs
@@ -6,6 +6,7 @@ namespace Decisions
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Bob's Big Giveaway");
             Console.Write("Chose a door: 1, 2 or 3: ");
 
@@ -29,6 +30,20 @@ namespace Decisions
             }
             Console.WriteLine(message);
             Console.ReadLine();
+            */
+
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Chose a door: 1, 2 or 3: ");
+
+            string userValue = Console.ReadLine();
+
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+            Console.Write("You won a ");
+            Console.Write(message);
+            Console.Write(".");
+            Console.ReadLine();
+
+
         }
     }
 }
