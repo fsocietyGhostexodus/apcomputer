db43cbedb82584598aef3e3ab492924da00d08d3
Jayden Bishop <jbpi8607@gmail.com>
2021-10-21 11:09:29 -0400

    hi

---
+++ Decisions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Chose a door: 1, 2 or 3: ");
+
+            string userValue = Console.ReadLine();
+            string message = "";
+            if (userValue == "1")
+                message = "You won a new car!";
+            
+
+            else if (userValue == "2")
+                 message = "You won a new boat!";
+                
+            
+
+            else if (userValue == "3")
+                 message = "You won a new cat!";
+            
+            else {
+                 message = "Sorry, we didn't understand.";
+                 message = message + " You Lose.";
+            }
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Variables/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Varibale Program: ");
+            Console.WriteLine();
+
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y); */
+
+            Console.WriteLine("What is your name? ");
+            Console.WriteLine("Type your first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            string myLastName;
+            Console.Write("Type your last name: ");
+            myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+
+
+        }
+    }
+}
