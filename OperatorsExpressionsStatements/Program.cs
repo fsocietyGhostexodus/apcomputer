90fb2d229b10e25b4dcac631c0faad1915cef539
Jayden Bishop <jbpi8607@gmail.com>
2021-10-22 11:19:36 -0400

    hi

--- OperatorsExpressionsStatements/Program.cs
+++ OperatorsExpressionsStatements/Program.cs
@@ -32,6 +32,59 @@ namespace OperatorsExpressionsStatements
             // Order of operations using parthesis
             x =  (x + y) * (a - b);
 
+            // There are many operators used to evaluate values ...
+
+            // Equality operator
+            if (x == y){
+
+            }
+
+            // Greater than operator
+            if (x > y){
+                
+            }
+
+            // Less than operator
+            if (x < y){
+
+            }
+
+            // Greater or equal operator
+            if (x >= y){
+
+            }
+
+            //Less than or equal to operator
+            if (x<= y){
+
+            }
+
+            // There are two "conditional" operators as well that can 
+            // be used to expand / enchance an evaluation ...
+            // ... and they can be combined together mutiple times.
+
+            // Conditional AND operator ...
+            if ((x > y) && (a > b)){
+
+            }
+
+            // Conditional OR operator...
+            if ((x > y) || (a > b)){
+
+            }
+
+            // Also, here's the in-line conditional operator we
+            // learned about in the previous lesson ...`
+            string message = (x == 1) ? "Car" : "Boat";
+
+            //Member access and Method invocation
+            Console.WriteLine("Hi");
+
+
+
+
+
+
                
         }
     }
