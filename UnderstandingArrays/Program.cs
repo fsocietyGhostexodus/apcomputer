e7143f4ab921ba2395f46baf4efaa48043d25122
Jayden Bishop <jbpi8607@gmail.com>
2021-10-25 11:22:46 -0400

    hi

--- UnderstandingArrays/Program.cs
+++ UnderstandingArrays/Program.cs
@@ -34,7 +34,31 @@ namespace UnderstandingArrays{
             Console.WriteLine(numbers.Length);
             Console.ReadLine();
             */
+
+            //int [] numbers = new int[] {4, 8, 15, 16, 23, 42};
             
+            //string[] names = new string[] {"Eddie", "Alex", "Michael", "David Lee"};
+            /*
+            for (int i = 0; i < names.Length; i++){
+                Console.WriteLine(names[i]);
+            }
+            */
+            /*
+            foreach (string name in names){
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+            */
+
+            string zig = "You can get what you want out of life " +
+                "if you help enough other people get what they want.";
+            char[] charArray = zig.ToCharArray();
+            Array.Reverse(charArray);
+            
+            foreach (char zigChar in charArray){
+                Console.Write(zigChar);
+            }
+            Console.ReadLine();
         }
         
     }
